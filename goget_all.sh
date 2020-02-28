8384b8a16f6996053237876e0d1db2da3552de5d
EricKIm <kimhi65@gmail.com>
2020-02-28 15:54:29 +0900

    200228-1554-Kim

--- goget_all.sh
+++ goget_all.sh
@@ -5,7 +5,7 @@ go get github.com/pilu/fresh
 go get github.com/labstack/echo
 go get github.com/go-sql-driver/mysql
 go get github.com/dabory/abango
-go get github.com/dabory/kafka-docker
+git clone https://github.com/dabory/kafka-docker
 go get github.com/dabory/svc-abango 
 go get github.com/dabory/end-abango 
 go get github.com/dabory/xorm
